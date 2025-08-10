2ee4f06837f535c55b77be554f897e7d60afad95
Disha Uplanchiwar <dishauplanchiwar@gmail.com>
2025-08-10 19:44:46 +0530

    all update files

--- Flask/app.py
+++ Flask/app.py
@@ -22,6 +22,19 @@ input_fields = [
 def home():
     return render_template("home.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+
+
+
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
